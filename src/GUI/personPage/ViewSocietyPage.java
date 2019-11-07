ebacb680421c00473689dc9379facb1d2ef4e40e
lisawerner <lisawerner@uni-koblenz.de>
2019-11-07 20:55:39 +0100

    Society Update: View Society by Races

--- src/GUI/personPage/ViewSocietyPage.java
+++ src/GUI/personPage/ViewSocietyPage.java
@@ -2,6 +2,7 @@ package GUI.personPage;
 
 import GUI_components.Page;
 import GUI_components.StructureCard;
+import book.Book;
 
 public class ViewSocietyPage extends Page {
 	private static final long serialVersionUID = 1L;
@@ -10,6 +11,10 @@ public class ViewSocietyPage extends Page {
 		super("Society: Persons, Relationships, ...");
 	
 		this.addCard(new StructureCard("Familiar Society", new SocietyFamiliarCard()));
+		
+		if(Book.getInstance().getSociety().isRaceSystemActivated()) {
+			this.addCard(new StructureCard("Race Society", new SocietyRaceCard()));
+		}
 		
 		setMenu(new PersonMenu());
 	}
