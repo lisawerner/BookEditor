348daf6b5043c936be2a4b281f4e9b8231e4a5b8
lisawerner <lisawerner@uni-koblenz.de>
2020-07-08 20:43:19 +0200

    Clean up

--- src/global/SerializedObject.java
+++ src/global/SerializedObject.java
@@ -8,12 +8,6 @@ public class SerializedObject {
 		my_uID = new ObjectID(this.getClass().getName());
 	}
 	
-	public void setID(){ //TODO delete this function ???
-		if(my_uID == null){
-			my_uID = new ObjectID(this.getClass().getName());
-		}
-	}
-	
 	public ObjectID getID() {
 		return my_uID;
 	}
